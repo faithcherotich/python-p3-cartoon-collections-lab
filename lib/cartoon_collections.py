2d764070d69a75c45d6475bc7a021e8cab9c538a
faithcherotich <faithcherotich910@gmail.com>
2024-09-02 15:38:32 +0300

    commit

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, dwarf in enumerate(dwarves, start=1):
+        print(f"{index}. {dwarf}") 
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + "!" for call in planeteer_calls]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+results= summon_captain_planet(["carrot", "cucumber", "pepper"])
+print(results)
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+
+def find_the_cheese(items):
+    cheeses = ["cheddar","gouda","camembert"]
+    for item in items:
+         if item in cheeses:
+            return item
+    return None
+
